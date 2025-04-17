7816c6cd58a03052b3c7ec0b87c47adafd53f653
diegoaislasc <diegoaislas03@gmail.com>
2025-04-17 16:48:46 -0600

    connection to database succesful

--- app/database.py
+++ app/database.py
@@ -1,9 +1,13 @@
 # connect and log into a PostgreSQL database
 # https://www.pg4e.com/lectures/06-Python.php
+
 import psycopg2
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
 
@@ -13,19 +17,19 @@ dbname = os.getenv("PG_DB")
 user = os.getenv("PG_USER")
 password = os.getenv("PG_PASS")
 
-print("HOST:", host)
-print("DB:", dbname)
-print("USER:", user)
-print("PASS:", password)
 
-# pg connection
-conn = psycopg2.connect(
-    host=host,
-    database=dbname,
-    user=user,
-    password=password,
-    connect_timeout=3
-)
+Base = declarative_base()
+engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}/{dbname}')
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 print("Conexión exitosa")
 
--- app/main.py
+++
@@ -1,9 +0,0 @@
-from app.database import conn
-
-cur = conn.cursor()
-
-cur.execute('SELECT *'
-            ' FROM '
-            'artist;')
-row = cur.fetchone()
-print('Found', row)
